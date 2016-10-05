32e5be5213901299af164eeb4fac699a56e2026e
@ <yuezhaoyang7449@163.com>
2016-10-05 19:48:04 +0800

    add TabFragment

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Android开发Widget集合
 
 由于时间越往后自己写的Demo越来越多，如果每个都分别提交git，越来越多会很难管理，所以专门开个项目来管理这些Demo
+
+Banner [https://7449.github.io/Android_首页banner封装](https://7449.github.io/Android_%E9%A6%96%E9%A1%B5banner%E5%B0%81%E8%A3%85/)
+
---
+++ TabFragment/app/src/main/java/github/com/tabfragment/BaseFragment.java
@@ -0,0 +1,17 @@
+package github.com.tabfragment;
+
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+
+/**
+ * by y on 2016/10/5.
+ */
+
+public class BaseFragment extends Fragment {
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        //避免横竖屏切换时页面重叠
+    }
+}
