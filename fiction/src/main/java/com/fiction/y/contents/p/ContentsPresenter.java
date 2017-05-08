12bb627e23870956a90771d113d699f910764e42
@ <yuezhaoyang7449@163.com>
2017-05-08 17:55:48 +0800

    remove tempfile

--- fiction/src/main/java/com/fiction/y/contents/p/ContentsPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.fiction.y.contents.p;
-
-/**
- * by y on 2017/1/8.
- */
-
-public interface ContentsPresenter {
-    void startContents(String url);
-}
--- fiction/src/main/java/framework/data/Constant.java
+++
@@ -1,9 +0,0 @@
-package framework.data;
-
-/**
- * by y on 2016/7/26.
- */
-public class Constant {
-
-
-}
