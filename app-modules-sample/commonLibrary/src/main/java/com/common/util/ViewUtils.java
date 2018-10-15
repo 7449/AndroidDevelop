7ff2a535d99032187f68bdd4ec22b1bca506b040
@ <yuezhaoyang7449@163.com>
2018-10-15 14:31:38 +0800

    add ScreenStatusUtils

--- app-modules-sample/commonLibrary/src/main/java/com/common/util/ViewUtils.java
+++ app-modules-sample/commonLibrary/src/main/java/com/common/util/ViewUtils.java
@@ -1,5 +1,8 @@
 package com.common.util;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.View;
 
 /**
@@ -23,6 +26,14 @@ public class ViewUtils {
         for (View view : views) {
             view.setVisibility(View.INVISIBLE);
         }
+    }
+
+    public static Bitmap toBitmap(View view, int bitmapWidth, int bitmapHeight) {
+        Bitmap bitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.WHITE);
+        view.draw(canvas);
+        return bitmap;
     }
 
 }
