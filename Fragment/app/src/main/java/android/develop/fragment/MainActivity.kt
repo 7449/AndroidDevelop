e6899932d9f5e08653a349e5c2e9dd2521a2ed0e
7449 <yuezhaoyang7449@163.com>
2019-03-23 22:45:21 +0800

    update viewpager sample

--- Fragment/app/src/main/java/android/develop/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/android/develop/fragment/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import fragment.develop.android.lazy.LazyMainActivity
 import fragment.develop.android.tab.TabMainActivity
 import fragment.develop.android.viewpager.ViewPagerMainActivity
+import fragment.develop.android.viewpager.two.ViewPager2MainActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,6 @@ class MainActivity : AppCompatActivity() {
         tabFragment.setOnClickListener { startActivity(Intent(this, TabMainActivity::class.java)) }
         lazyFragment.setOnClickListener { startActivity(Intent(this, LazyMainActivity::class.java)) }
         viewPagerFragment.setOnClickListener { startActivity(Intent(this, ViewPagerMainActivity::class.java)) }
+        viewPagerFragment2.setOnClickListener { startActivity(Intent(this, ViewPager2MainActivity::class.java)) }
     }
 }
