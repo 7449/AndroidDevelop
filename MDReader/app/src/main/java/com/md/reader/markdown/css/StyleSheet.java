75028963069c4dc0f7df91b980785439146cc4f6
@ <yuezhaoyang7449@163.com>
2019-12-04 18:16:45 +0800

    zip

--- MDReader/app/src/main/java/com/md/reader/markdown/css/StyleSheet.java
+++
@@ -1,5 +0,0 @@
-package com.md.reader.markdown.css;
-
-public interface StyleSheet {
-    String toHTML();
-}
--- MDReader/app/src/main/java/com/md/reader/markdown/js/JavaScript.java
+++
@@ -1,5 +0,0 @@
-package com.md.reader.markdown.js;
-
-public interface JavaScript {
-    String toHTML();
-}
